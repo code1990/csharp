11e1f18afdaa947177d23bd033f8c65c90744291
code1990 <s1332177151@sina.com>
2019-12-26 22:00:57 +0800

    test

---
+++ test/test/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Console.Write("Hello CShap Unit Test");
+        }
+    }
+}
