a78fd6863f525b45d6d261614b50baa9bc978355
code1990 <s1332177151@sina.com>
2019-12-27 23:08:53 +0800

    plan

---
+++ test/test/HellWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace test
+{
+    public class HellWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.WriteLine("Hello CSharp");
+            Console.ReadKey();
+        }
+    }
+}
