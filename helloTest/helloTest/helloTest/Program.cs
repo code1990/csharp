bd679dfa0247847d1f083b5f99aa2b121b063eff
code1990 <s1332177151@sina.com>
2020-06-22 22:21:31 +0800

    init

---
+++ helloTest/helloTest/helloTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello world");
+            Chapter01.Test();
+        }
+    }
+}
